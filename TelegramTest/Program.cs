484035af15371346202493fa4a66afe81029a0a0
Vitalii <bamnup12@gmail.com>
2016-04-23 23:42:50 +0200

    Add project files.

---
+++ TelegramTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace TelegramTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testApi();
+            Console.ReadKey();
+        }
+
+        static async void testApi()
+        {
+            var bot = new Telegram.Bot.Api("213196674:AAEFdKzcvd_Ln-2VpwKKzQ4ASE0oP6osvb8");
+            var me = await bot.GetMe();
+            var updates = await bot.GetUpdates();
+            var firstOrDefault = updates.FirstOrDefault();
+            var id = firstOrDefault.Message.Chat.Id;
+            bot.MessageReceived += (sender, args) => { bot.SendTextMessage(id, "Hi"); };
+            Console.WriteLine("Hello my name is " + me.FirstName);
+        }
+
+    }
+}
